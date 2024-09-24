f357bcda45cbce839e54ff92912d9c0dcc0f4c92
Aleksandra Nenadic <a.nenadic@manchester.ac.uk>
2024-09-24 09:22:34 +0100

    Update eva_data_analysis.py

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,7 +1,7 @@
 # https://data.nasa.gov/resource/eva.json
 data_f = open('./eva-data.json', 'r')
 data_t = open('./eva-data.csv','w')
-g_file = 'myplot.png'
+g_file = './cumulative_eva_graph.png'   
 
 fieldnames = ("EVA #", "Country", "Crew    ", "Vehicle", "Date", "Duration", "Purpose")
 
